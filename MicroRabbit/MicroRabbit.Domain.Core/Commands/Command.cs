e8d3f5e44ed59f36589d3abfbc4f367619ba52b3
Agner Macedo Paiva <agnermp@gmail.com>
2020-10-21 23:39:04 -0300

    Implemented interfaces and classes for domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command: Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
