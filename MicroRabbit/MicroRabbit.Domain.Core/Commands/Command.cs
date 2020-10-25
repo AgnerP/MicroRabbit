5426306d509e2ee7b3dff026ec3ecc8def2d4704
Agner Macedo Paiva <agnermp@gmail.com>
2020-10-25 03:00:14 -0300

    Implementation of RabbitMQ Bus completed

--- MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -10,7 +10,7 @@ namespace MicroRabbit.Domain.Core.Commands
     {
         public DateTime Timestamp { get; protected set; }
         protected Command()
-        {
+        {            
             Timestamp = DateTime.Now;
         }
     }
